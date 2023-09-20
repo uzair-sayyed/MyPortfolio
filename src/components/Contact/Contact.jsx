f7b6f2827d2c367829ed4a26cee07523152c856c
Uzair Sayyed <sayeduzair54321@gmail.com>
2023-09-21 02:50:52 +0530

    added form validation

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,21 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Contact.css";
 function Contact() {
   const [name, setName] = useState("");
+  const [nameIsValid, setNameIsValid] = useState();
   const [email, setEmail] = useState("");
+  const [emailIsValid, setEmailIsValid] = useState();
   const [phonenumber, setPhonenumber] = useState("");
+  const [phoneIsValid, setPhoneIsValid] = useState();
   const [message, setMessage] = useState("");
+  const [messageIsValid, setMessageIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+    setFormIsValid(
+      name.trim().length >= 4 &&
+        email.includes("@") &&
+        phonenumber.toString().trim().length >= 6 &&
+        message.trim().length >= 20
+    );
+  }, [name, email, phonenumber, message]);
+
   const nameChangeHandler = (e) => {
     setName(e.target.value);
   };
+  const validateNameHandler = () => {
+    setNameIsValid(name.trim().length >= 4);
+  };
+
   const emailChangeHandler = (e) => {
     setEmail(e.target.value);
   };
+  const validateEmailHandler = () => {
+    setEmailIsValid(email.includes("@"));
+  };
+
   const phoneNumberChangeHandler = (e) => {
     setPhonenumber(e.target.value);
   };
+  const validatePhoneHandler = () => {
+    setPhoneIsValid(phonenumber.toString().trim().length >= 6);
+  };
+
   const messageChangeHandler = (e) => {
     setMessage(e.target.value);
+  };
+  const validateMessageHandler = () => {
+    setMessageIsValid(message.trim().length >= 20);
   };
   const submitHandler = (e) => {
     setTimeout(() => {
@@ -34,57 +64,71 @@ function Contact() {
           action={"https://formspree.io/f/meqbgvpv"}
           method={"post"}
         >
-          <div className='group'>
+          <div
+            className={`${"group"} ${nameIsValid === false ? "invalid" : ""}`}
+          >
             <label htmlFor='name'>Name</label>
             <input
-              onChange={nameChangeHandler}
               type='text'
               name='Name'
               id='name'
               placeholder='Enter Your Name'
+              onBlur={validateNameHandler}
+              onChange={nameChangeHandler}
               value={name}
               required
             />
           </div>
-          <div className='group'>
+          <div
+            className={`${"group"} ${emailIsValid === false ? "invalid" : ""}`}
+          >
             <label htmlFor='email'>Email</label>
             <input
-              onChange={emailChangeHandler}
               type='email'
               name='Email'
               id='email'
               placeholder='Enter Your Email'
               value={email}
+              onBlur={validateEmailHandler}
+              onChange={emailChangeHandler}
               required
             />
           </div>
-          <div className='group'>
+          <div
+            className={`${"group"} ${phoneIsValid === false ? "invalid" : ""}`}
+          >
             <label htmlFor='phonenumber'>Phone Number</label>
             <input
-              onChange={phoneNumberChangeHandler}
               type='number'
               name='Phone Number'
               id='phonenumber'
               placeholder='Enter Your Phone Number'
+              onChange={phoneNumberChangeHandler}
+              onBlur={validatePhoneHandler}
               value={phonenumber}
               required
             />
           </div>
-          <div className='group'>
+          <div
+            className={`${"group"} ${
+              messageIsValid === false ? "invalid" : ""
+            }`}
+          >
             <label htmlFor='message'>Message</label>
             <textarea
-              onChange={messageChangeHandler}
               name='Message'
               id='message'
               cols='30'
               rows='10'
               placeholder='Enter Your Message'
               value={message}
+              onChange={messageChangeHandler}
+              onBlur={validateMessageHandler}
               required
             ></textarea>
           </div>
           <div className='button-div'>
-            <button type='submit'>
+            <button type='submit' disabled={!formIsValid}>
               <p>Submit</p>
             </button>
           </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Main.css";
 import MyPic from "../../assets/myPic.png";
 import { Typewriter, Cursor } from "react-simple-typewriter";
 function Main() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState("");
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  useEffect(() => {
+    setFormIsValid(
+      enteredEmail.includes("@") && enteredPassword.trim().length > 6
+    );
+  }, [enteredEmail, enteredPassword]);
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+
+  const validateEmailHandler = () => {
+    setEmailIsValid(enteredEmail.includes("@"));
+  };
+
+  const validatePasswordHandler = () => {
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
   return (
     <main id='main' className='container'>
       <div className='main'>
